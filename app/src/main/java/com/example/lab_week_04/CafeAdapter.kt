c56f2a344d421a45a8aeed8100198e5981700e53
MightyMike28 <anandamieko@gmail.com>
2025-09-21 12:32:36 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -10,13 +10,21 @@ val TABS_FIXED = listOf(
     R.string.janjijiwa_title,
     R.string.kopikenangan_title,
 )
+
 class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
+
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
-    override fun createFragment(position: Int): Fragment
-    {
-        return CafeDetailFragment()
+
+    override fun createFragment(position: Int): Fragment {
+        val content = when (position) {
+            0 -> R.string.starbucks_desc
+            1 -> R.string.janjijiwa_desc
+            2 -> R.string.kopikenangan_desc
+            else -> R.string.app_name
+        }
+        return CafeDetailFragment.newInstance(content)
     }
 }
